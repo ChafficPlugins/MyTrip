4ad210169c83800fa4ceb88d32bc73a6c5367173
Chafficui <62988998+Chafficui@users.noreply.github.com>
2020-04-01 17:25:50 +0200

    Update KillerCommands.java

--- src/de/chaffic/MyTrip/KillerCommands.java
+++ src/de/chaffic/MyTrip/KillerCommands.java
@@ -1,159 +1,204 @@
 package de.chaffic.MyTrip;
 
+import java.io.File;
+import java.util.Arrays;
 import java.util.logging.Logger;
-import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import org.bukkit.potion.PotionEffect;
 
-public class KillerCommands
-  implements CommandExecutor
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class Main
+  extends JavaPlugin
 {
 	
-	private Main plugin;
+	public static ItemStack wheatd = new ItemStack(Material.WHEAT);
+    public static ItemStack suagrd = new ItemStack(Material.SUGAR);
+	public static ItemStack paperd = new ItemStack(Material.PAPER);
+	public static ItemStack sugarcaned = new ItemStack(Material.SUGAR_CANE);
+	public static ItemStack mushroomd = new ItemStack(Material.RED_MUSHROOM);
+
+
+
 	
-	 public KillerCommands(Main plugin)
-	  {
-	    setPlugin(plugin);
-	  }
-	  
-	  public Main getPlugin()
-	  {
-	    return this.plugin;
-	  }
-	  
-	  public void setPlugin(Main plugin)
-	  {
-	    this.plugin = plugin;
-	  }
-	  
+	public static String weed = "Weed: Nur eine Einsteigerdroge??";
+	public static String coke = "Kokain: Komm zieh es durch die Nase!";
+	public static String acid = "LSD: Macht Spaß, aber kann mieß enden...";
+	public static String speziale = "Heroin: Wer weiß, was kommt?";
+	public static String shrooms = "Pilz: Champignon oder Steinpilz???";
   Logger logger = Logger.getLogger("Minecraft");
-  public final String prefix = ChatColor.RED + "====" + ChatColor.BLACK + "[" + ChatColor.DARK_RED + "Version: 3.0.1 (Alpha)" + ChatColor.BLACK + "]" + ChatColor.RED + "====";
+  File f = new File(this.getDataFolder() + "/");
   
-  final String dash = ChatColor.GRAY + "- ";
-  public final static String prefix2 = ChatColor.BLACK + "[" + ChatColor.DARK_RED + "MT" + ChatColor.BLACK + "] " + ChatColor.RESET;
-  final String dash1 = ChatColor.GOLD + "- " + ChatColor.GRAY;
-  final String perm = ChatColor.RED + "You don't have the right permission";
-  
-  
-  public boolean onCommand(CommandSender sender, Command command, String Commandlabel, String[] args)
+  public void onEnable()
   {
-    if (command.getName().equalsIgnoreCase("mytrip"))
-    {
-      if (args.length == 0)
-      {
-        if ((sender instanceof Player))
-        {
-          Player p = (Player)sender;
-          if (p.hasPermission("mytrip.main"))
-          {
-            p.sendMessage(ChatColor.RED + "Use the the sub commands listed below:");
-            p.sendMessage(ChatColor.GRAY + "- " + ChatColor.WHITE + "Ex. " + ChatColor.RED + "/" + ChatColor.WHITE + "mytrip help");
-            p.sendMessage(ChatColor.GRAY + "- " + ChatColor.WHITE + "Ex. " + ChatColor.RED + "/" + ChatColor.WHITE + "mytrip remove");
-            p.sendMessage(ChatColor.GRAY + "- " + ChatColor.WHITE + "Ex. " + ChatColor.RED + "/" + ChatColor.WHITE + "mytrip reload");
-          }
-          else
-          {
-            p.sendMessage(this.perm);
-          }
-        }
-        else
-        {
-          sender.sendMessage(ChatColor.RED + "Use the the sub commands listed below:");
-          sender.sendMessage(ChatColor.GRAY + "- " + ChatColor.WHITE + "Ex. " + ChatColor.RED + "/" + ChatColor.WHITE + "mytrip help");
-          sender.sendMessage(ChatColor.GRAY + "- " + ChatColor.WHITE + "Ex. " + ChatColor.RED + "/" + ChatColor.WHITE + "mytrip remove");
-          sender.sendMessage(ChatColor.GRAY + "- " + ChatColor.WHITE + "Ex. " + ChatColor.RED + "/" + ChatColor.WHITE + "mytrip reload");
-        }
-      }
-      else if (args[0].equalsIgnoreCase("remove"))
-      {
-        if ((sender instanceof Player))
-        {
-          Player p = (Player)sender;
-          if (!p.getActivePotionEffects().isEmpty())
-          {
-            if (p.hasPermission("mytrip.remove"))
-            {
-              for (PotionEffect effect : p.getActivePotionEffects()) {
-                p.removePotionEffect(effect.getType());
-              }
-              p.sendMessage(prefix2 + ChatColor.AQUA + "Sobered Up!");
-            }
-            else
-            {
-              p.sendMessage(this.perm);
-            }
-          }
-          else {
-            p.sendMessage(ChatColor.MAGIC + "|||" + ChatColor.RESET + "you need drugs" + ChatColor.MAGIC + "|||");
-          }
-        }
-        else
-        {
-          sender.sendMessage("Only players may use this command.");
-        }
-      }
-      else if (args[0].equalsIgnoreCase("help")) {
-        if ((sender instanceof Player))
-        {
-          Player p = (Player)sender;
-          if (p.hasPermission("mytrip.help"))
-          {
-            sender.sendMessage(this.prefix);
-            sender.sendMessage(this.dash + ChatColor.GREEN + "Wheat " + ChatColor.WHITE + "(Weed)");
-            sender.sendMessage(this.dash + ChatColor.GREEN + "Sugar " + ChatColor.WHITE + "(Kokain)");
-            sender.sendMessage(this.dash + ChatColor.GREEN + "Paper " + ChatColor.WHITE + "(LSD)");
-          }
-          else
-          {
-            p.sendMessage(this.perm);
-          }
-        }
-        else
-        {
-          sender.sendMessage(this.prefix);
-          sender.sendMessage(this.dash + ChatColor.GREEN + "Wheat " + ChatColor.WHITE + "(Weed)");
-          sender.sendMessage(this.dash + ChatColor.GREEN + "Sugar " + ChatColor.WHITE + "(Kokain)");
-          sender.sendMessage(this.dash + ChatColor.GREEN + "Paper " + ChatColor.WHITE + "(LSD)");
-        }
-      }    else if (args[0].equalsIgnoreCase("reload")) {
-          try
-          {
-            if ((sender instanceof Player))
-            {
-              Player p = (Player)sender;
-              if (p.hasPermission("mytrip.reload"))
-              {
-                p.sendMessage(ChatColor.GREEN + "Reloading config...");
-                this.plugin.reloadConfig();
-                p.sendMessage(ChatColor.GREEN + "Reloaded Config");
-              }
-              else
-              {
-                p.sendMessage(this.perm);
-              }
-            }
-            else
-            {
-             this.plugin.reloadConfig();
-            }
-          }
-          catch (Exception e)
-          {
-        	  if(sender instanceof Player) {
-        		  Player p = (Player) sender;
-        		  p.sendMessage(ChatColor.DARK_RED + "ERROR!");
-        		  p.sendMessage(ChatColor.BOLD + "Check the Console for the stack trace.");
-        	  }
-            sender.sendMessage(ChatColor.RED + "Error: " + ChatColor.WHITE + "Config failed to load.");
-            sender.sendMessage(ChatColor.GREEN + "If this keeps happening tell me on Spigot");
-            sender.sendMessage(ChatColor.GREEN + "DON'T FORGET TO BRING THIS ERROR!");
-            e.printStackTrace();
-          }
-        }
-    }
-    return true;
+
+	  //Adding in the Recipes for the Drugs
+	  WeedRec();
+	  CokeRec();
+	  AcidRec();
+	  SugarcaneRec();
+	  ShroomsRec();
+	  
+	  //Registering Commands and Events
+	    getCommand("mytrip").setExecutor(new KillerCommands(this));
+	    getServer().getPluginManager().registerEvents(new Items(this), this);
+	  
+	    //Checking and Creating a folder for the plugin
+	  if(!f.exists()) {
+		  f.mkdir();
+	  }else {
+		  System.out.println("Folder already exists.");
+	  }
+	  
+	 //Checking and loading in plugin config
+    getConfig().options().header("MyTrip Config.");
+    loadConfiguration();
   }
+  
+  
+  //What is inside the config
+  public void loadConfiguration()
+  {
+    String wheat = "mytrip.Toggle.wheat";
+    String suagr = "mytrip.Toggle.sugar";
+    String paper = "mytrip.Toggle.paper";
+    String sugarcane = "mytrip.Toggle.sugarcane";
+	String red_mushroom = "mytrip.Toggle.red_mushroom";
+    String effect = "mytrip.Effect.length";
+    String effect2 = "mytrip.Effect.confusion.length";
+
+    
+    getConfig().addDefault(wheat, Boolean.valueOf(true));
+    getConfig().addDefault(suagr, Boolean.valueOf(true));
+    getConfig().addDefault(paper, Boolean.valueOf(true));
+    getConfig().addDefault(sugarcane, Boolean.valueOf(true));
+    getConfig().addDefault(red_mushroom, Boolean.valueOf(true)); 
+    
+    getConfig().addDefault(effect, Integer.valueOf(5220));
+    getConfig().addDefault(effect2, Integer.valueOf(2600));
+    
+    getConfig().options().copyDefaults(true);
+    saveConfig();
+  }
+  //Weed Rezept
+  public void WeedRec() {
+	  ItemMeta meta = wheatd.getItemMeta();
+	  
+	  
+	  meta.setDisplayName(ChatColor.GREEN+ "Weed");
+	  meta.setLore(Arrays.asList(weed));
+	  meta.addEnchant(Enchantment.DAMAGE_ALL, 1, true);
+	  meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+	  wheatd.setItemMeta(meta);
+	  
+	  NamespacedKey key = new NamespacedKey(this, "mytrip_weed");
+	  ShapedRecipe recipe = new ShapedRecipe(key, wheatd);
+	  
+	  recipe.shape(" W ",
+			       " W ",
+			       " W ");
+	  
+	  recipe.setIngredient('W', Material.WHEAT);
+	  
+	  Bukkit.addRecipe(recipe);
+  }
+  //Kokain Rezept
+  public void CokeRec() {
+	  ItemMeta meta = suagrd.getItemMeta();
+	  
+	  meta.setLore(Arrays.asList(coke));
+	  meta.setDisplayName(ChatColor.GOLD + "Kokain");
+	  meta.addEnchant(Enchantment.DAMAGE_ALL, 1, true);
+	  meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+	  suagrd.setItemMeta(meta);
+	  
+	  NamespacedKey key = new NamespacedKey(this,"mytrip_coke");
+	  ShapedRecipe recipe = new ShapedRecipe(key, suagrd);
+	  
+	  recipe.shape(" S ",
+			       " S ",
+			       " S ");
+	  
+	  recipe.setIngredient('S', Material.SUGAR);
+	  
+	  Bukkit.addRecipe(recipe);
+	  }
+  //LSD Rezept
+  public void AcidRec() {
+	  ItemMeta meta = paperd.getItemMeta();
+
+	  meta.setLore(Arrays.asList(acid));
+	  meta.setDisplayName(ChatColor.BLUE + "LSD");
+	  meta.addEnchant(Enchantment.DAMAGE_ALL, 1, true);
+	  meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+	  paperd.setItemMeta(meta);
+	  
+	  NamespacedKey key = new NamespacedKey(this, "mytrip_acid");
+	  ShapedRecipe recipe = new ShapedRecipe(key, paperd);
+	  
+	  recipe.shape(" P ",
+			       " P ",
+			       " P ");
+	  
+	  recipe.setIngredient('P', Material.PAPER);
+	  
+	  Bukkit.addRecipe(recipe);
+  	}
+  //Speziale Rezept
+  public void SugarcaneRec() {
+	  ItemMeta meta = sugarcaned.getItemMeta();
+
+	  meta.setLore(Arrays.asList(speziale));
+	  meta.setDisplayName(ChatColor.DARK_PURPLE + "Heroin");
+	  meta.addEnchant(Enchantment.DAMAGE_ALL, 1, true);
+	  meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+	  sugarcaned.setItemMeta(meta);
+
+	  NamespacedKey key = new NamespacedKey(this, "mytrip_sugarcane");
+	  ShapedRecipe recipe = new ShapedRecipe(key, sugarcaned);
+
+	  recipe.shape(" U ",
+			       " U ",
+			       " U ");
+
+	  recipe.setIngredient('U', Material.SUGAR_CANE);
+
+	  Bukkit.addRecipe(recipe);
+
+  	}
+  public void ShroomsRec() {
+	  ItemMeta meta = mushroomd.getItemMeta();
+
+	  meta.setLore(Arrays.asList(shrooms));
+	  meta.setDisplayName(ChatColor.RED + "Pilz");
+	  meta.addEnchant(Enchantment.DAMAGE_ALL, 1, true);
+	  meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+	  mushroomd.setItemMeta(meta);
+
+	  NamespacedKey key = new NamespacedKey(this, "mytrip_red_mushroom");
+	  ShapedRecipe recipe = new ShapedRecipe(key, mushroomd);
+
+	  recipe.shape(" R ",
+			       " R ",
+			       " R ");
+
+	  recipe.setIngredient('R', Material.RED_MUSHROOM);
+
+	  Bukkit.addRecipe(recipe);
+
+  	}
+  
+
+
+//Nothing
+  public void onDisable() {}
+  
 }
